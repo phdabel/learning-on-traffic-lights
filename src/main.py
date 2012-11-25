5a1dacc98dccb458c60173bfd70a3d3d3e92f227
phd.abel <phd.abel@cd2077b9-452d-439c-2b3f-d93614fa49d0>
2012-11-25 19:19:05 +0000

    algoritmo funcionando, falta plotar resultados

--- src/main.py
+++ src/main.py
@@ -50,18 +50,46 @@ if __name__ == '__main__':
     #vincula av_table com o agente
     learner = Q(0.5,0.0)
     learner._setExplorer(BoltzmannExplorer())
-    agent = LowLevelAgent("B7", av_table, learner)
+    trafficlightsids = ["B2","B3","B4","B5","B6","B7",
+                        "C2","C3","C4","C5","C6","C7",
+                        "D2","D3","D4","D5","D6","D7",
+                        "E2","E3","E4","E5","E6","E7",
+                        "F2","F3","F4","F5","F6","F7",
+                        "G2","G3","G4","G5","G6","G7"]
+    
+    agent = []
+    
+    for tls in trafficlightsids:
+        agent.append(LowLevelAgent(tls, av_table, learner))
+    #agent = LowLevelAgent("B7", av_table, learner)
 
     #definir ambiente
     env = TrafficLights()
+    tasks = []
     #definir tarefa - interacao com ambiente
-    task = Plan(env, agent)
+    for a in agent:
+        tasks.append(Plan(env, a))
+    #task = Plan(env, agent)
     
     #experimentar
-    experiment = Experiment(task,agent)
-
+    experiments = []
+    for x in range(len(agent) - 1):
+        experiments.append(Experiment(tasks[x], agent[x]))
+    
+    #experiment = Experiment(tasks,agent)
+    ct = 0
     for i in range(20000):
+        ct =+ 1
         if(random.random() <= prob):
             rota = route.getRandomRoute()
             carro.append(Vehicle(str(i), rota[0]))
             traci.simulationStep()
+        if(ct == 60):
+            ct = 0
+            for i in range(500):
+                for e in experiments:
+                    e.doInteractions(1)
+                    for a in agent:
+                        a.learn()
+                        a.reset()
+        
